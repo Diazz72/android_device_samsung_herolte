81b87f2ccbb2852cec97c3e7c8d434d0a6ee2500
Diazz72 <balkan.clash@gmail.com>
2017-01-17 17:50:51 +0100

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
+# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/universal8890
+TARGET_KERNEL_CONFIG := exynos8890-herolte_defconfig
+
+# DT/Boot/Recovery Image
+TARGET_DTS_NAMES := exynos8890-herolte_eur_open
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 41943040
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 48234496
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 4508876800
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27044851712
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 209715200
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_RECOVERY_FSTAB := device/samsung/hero-common/ramdisk/fstab.samsungexynos8890
+
+# RIL
+BOARD_MODEM_TYPE := ss333
+
+# Recovery
+TARGET_OTA_ASSERT_DEVICE := heroltebmc,herolteskt,heroltektt,heroltelgt,heroltexx,herolte
+
+# Inherit common board flags
+include device/samsung/hero-common/BoardConfigCommon.mk
